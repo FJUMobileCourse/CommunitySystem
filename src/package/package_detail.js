4df1ec20501837748b3d967defffdd9152ed34b7
張嘉程 <406402153@gapp.fju.edu.tw>
2020-12-14 23:17:04 +0800

    新增home和detail檔案

---
+++ src/package/package_detail.js
@@ -0,0 +1,2 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Navigator } from 'react-native';
---
+++ src/package/package_home.js
@@ -0,0 +1,3 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Navigator } from 'react-native';
+import package_detail from './package_detail';
