f5c1c896695496ec0b379a0c97c21f931535eb06
張嘉程 <406402153@gapp.fju.edu.tw>
2020-12-23 03:40:46 +0800

    .

--- src/package/PackageHome.js
+++ src/package/PackageHome.js
@@ -1,6 +1,32 @@
-
-import { StyleSheet, View, Text, Navigator, FlatList, Button, Modal} from 'react-native';
 import React, { useState, Component, useEffect } from 'react';
+import { FlatList, View, Text, Button, Modal } from 'react-native';
+import { Container, Content, Card, CardItem, Left, Right, Body, Icon, Thumbnail, Fab } from 'native-base';
+import styles from '../styles';
+import AddPost from './PackageDetail';
+import axios from 'axios';
+import Moment from 'moment';
+import {axios_config, url} from '../Config';
+
+export default function Index ({ Navigator }) {
+
+    return (
+        
+        <View>
+            <View style={{width:'100%', padding:10,height:100,backgroundColor:'grey'}}>
+                
+                    <Text style={{width:'50%', color:'white', fontSize:'30px', textAlign:'center', alignSelf:'flex-start'}}>待領取包裹</Text>
+            
+            
+                    <Text style={{width:'50%', color:'white', fontSize:'30px', textAlign:'center', alignSelf:'flex-end'}}>5</Text>
+            
+            </View>
+        </View>
+
+        
 
 
 
+
+       
+    )
+}
