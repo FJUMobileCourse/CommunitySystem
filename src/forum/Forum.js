a4efee946c3e8bb2456d7e7f4733681f6da605f8
ahsuanchen <ahsuan1027@gmail.com>
2020-12-30 11:19:10 +0800

    test

--- src/forum/Forum.js
+++ src/forum/Forum.js
@@ -3,19 +3,6 @@ import React, { useState, Component, useEffect } from 'react';
 import { FlatList, View, Text, Button, Modal } from 'react-native';
 import { Container, Content, Card, CardItem, Left, Right, Body, Icon, Thumbnail, Fab } from 'native-base';
 import styles from '../styles';
-<<<<<<< HEAD
-import AddPost from './AddPost';
-import axios from 'axios';
-import Moment from 'moment';
-import {axios_config, url} from '../Config';
-
-export default function Index({ navigation }) {
-    const finalUrl = url+'Forum?maxRecords=30&view=Grid%20view';
-    
-    const [posts, setPost] = useState([]);
-    const [modalVisible, setModalVisible] = useState(false);  
-
-=======
 import axios from 'axios';
 import Moment from 'moment';
 import { axios_config, url } from '../Config';
@@ -33,7 +20,6 @@ export default function Index({ navigation }) {
         //類似停止監聽
         return unsubscribe;
     }, [navigation]);
->>>>>>> home
 
     const renderItem = ({ item }) => (
         <Card>
@@ -50,22 +36,12 @@ export default function Index({ navigation }) {
                     </Body>
                 </Left>
             </CardItem>
-<<<<<<< HEAD
-            {/* 到時候導到PostDetail要隨著點擊的文不同，而導去不同的頁面 */}
-            <CardItem header bordered button onPress={() => navigation.navigate('PostDetail')}>
-=======
             <CardItem header bordered>
->>>>>>> home
                 <Body>
                     <Text>{item.fields.PostContent}</Text>
                 </Body>
             </CardItem>
-<<<<<<< HEAD
-            {/* 到時候導到PostDetail要隨著點擊的文不同，而導去不同的頁面 */}
-            <CardItem button onPress={() => navigation.navigate('PostDetail')}>
-=======
             <CardItem button onPress={() => GoToPostDetail(item.id)}>
->>>>>>> home
                 <Right style={styles.comment}>
                     <Icon active name="chatbubbles" style={{ marginRight: 5 }} />
                 </Right>
@@ -74,20 +50,6 @@ export default function Index({ navigation }) {
         </Card>
     )
 
-<<<<<<< HEAD
-    async function fetchData () {
-        const result = await axios.get(finalUrl,axios_config);
-        console.log(result);
-        setPost(result.data.records);
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, [modalVisible]);
-
-    function AddFormVisibleOrNot() {
-        setModalVisible(false);
-=======
     async function fetchData() {
         try {
             const result = await axios.get(finalUrl, axios_config);
@@ -100,25 +62,15 @@ export default function Index({ navigation }) {
 
     function GoToPostDetail(id) {
         navigation.navigate('PostDetail', { itemId: id });
->>>>>>> home
     }
 
     return (
         <Container style={styles.container}>
-<<<<<<< HEAD
-            <Content style={styles.item}>
-                <FlatList
-                    data={posts}
-                    renderItem={renderItem}
-                    keyExtractor={item => item.fields.Publisher} />
-            </Content>
-=======
             <FlatList
                 style={styles.item}
                 data={posts}
                 renderItem={renderItem}
                 keyExtractor={item => item.fields.PostID.toString()} />
->>>>>>> home
 
             <Fab
                 active="true"
@@ -126,18 +78,10 @@ export default function Index({ navigation }) {
                 containerStyle={{}}
                 style={{ backgroundColor: '#0080FF' }}
                 position="bottomRight"
-<<<<<<< HEAD
-                onPress={() => setModalVisible(true)}>
-                {/* Icon位置好像隨著手機不同會位移 */}
-                <Icon active name="add" style={{ fontSize: 35, marginTop: 10 }} />
-            </Fab>
-            <AddPost modalVisible={modalVisible} AddFormVisibleOrNot={AddFormVisibleOrNot} />
-=======
                 onPress={() => navigation.navigate('AddPost')}>
                 {/* Icon位置好像隨著手機不同會位移 */}
                 <Icon active name="add" style={{ fontSize: 35, marginTop: 10 }} />
             </Fab>
->>>>>>> home
         </Container>
     );
 }
