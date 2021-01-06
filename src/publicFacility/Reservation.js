6cd47187d5dda91edb0d18beb4793c4c8898075c
ahsuanchen <ahsuan1027@gmail.com>
2021-01-06 10:41:50 +0800

    new Reservation

--- src/publicFacility/Reservation.js
+++ src/publicFacility/Reservation.js
@@ -1,6 +1,6 @@
 import Icon from 'react-native-vector-icons/Ionicons';  
 import React, { useState, Component, useEffect } from 'react';
-import { FlatList, View, Text, Button} from 'react-native';
+import { FlatList, View, Text, Button , Image} from 'react-native';
 import { Container, Content, Card, CardItem, Left, Right, Body, Thumbnail, Fab } from 'native-base';
 import styles from '../styles';
 import Modal, {
@@ -26,26 +26,43 @@ export default function Reservation() {
 
     const renderItem = ({ item }) => (
         <Card>
-            <CardItem bordered style={{backgroundColor: "#D0D0D0"}}>
+            <CardItem bordered style={{backgroundColor: "#ACD6FF"}}>
                 <Body>
                     <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-                        <Text>設施名稱：{item.fields.FacilityName}</Text>
+                        <Left>
+                            <Text>設施名稱：{item.fields.FacilityName}</Text>
+                        </Left>
+                        <Right>
+                            <Image 
+                                source={{
+                                    uri : item.fields.Picture[0].url
+                                }}
+                                style={{
+                                    width: 200,
+                                    height: 100,
+                                }}
+                            />
+                        </Right>
                     </View>
                 </Body>
              </CardItem>
-             <CardItem header bordered>
+             <CardItem header >
                 <Body>
                     <Left>
                         <Text>設施資訊：</Text>
                         <Text>{item.fields.FacilityInfo}</Text>
-                        <Text></Text>
                         <Text>開放時間：</Text>
                         <Text>{Moment(item.fields.OpeningTime).format('HH:mm')} - {Moment(item.fields.EndTime).format('HH:mm')}</Text>
-                        {/* <Text></Text> */}
-                        <Text></Text>
                     </Left>
                     <Right>
-                        <image ></image>
+                        <Button style={{
+                                flex: 1 ,
+                                // flexDirection: 'row',
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}
+                            title="我要預約"
+                        />
                     </Right>
                 </Body>
             </CardItem>
@@ -53,18 +70,18 @@ export default function Reservation() {
     )
 
     async function fetchData () {
-        const result = await axios.get(finalUrl,axios_config);
-        console.log(result);
-        setPost(result.data.records);
+        const result = await axios.get(finalUrl , axios_config);
+        console.log(result.data);
+        setPost(result.data.records)
     }
 
     useEffect(() => {
         fetchData();
     }, []);
 
-    // function AddFormVisibleOrNot() {
-    //     setModalVisible(false);
-    // }
+    function AddFormVisibleOrNot() {
+        setModalVisible(false);
+    }
 
 //    function checkTime() {
 //        if (num==0){
@@ -78,14 +95,14 @@ export default function Reservation() {
 
     return (
       
-      <Container style={styles.container}>
-        <Content style={styles.item}>
-            <FlatList
-                data={posts}
-                renderItem={renderItem}
-                keyExtractor={item => item.fields.Publisher} />
-        </Content>
-            
-          </Container>
+        <Container style={styles.container}>
+            <Content style={styles.item}>
+                <FlatList
+                    data={posts}
+                    renderItem={renderItem}
+                    // keyExtractor={item => item.fields.Publisher}
+                />
+            </Content>   
+        </Container>
     );
 }
